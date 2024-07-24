80d73dab75a415b7f7d03d192d7a37b4cd93a2cd
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-07-24 02:39:05 +0200

    Refactor map display using onne write syscall

--- srcs/display_map.c
+++ srcs/display_map.c
@@ -6,30 +6,38 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 01:09:42 by ibertran          #+#    #+#             */
-/*   Updated: 2024/07/24 01:18:19 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/07/24 02:38:02 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+#include "ft_vector.h"
 #include "bsq.h"
 
 int	display_map(t_map *map)
 {
-	int	i;
-	int	j;
+	t_vector	buffer;
+	int			i;
+	int			j;
 
+	if (ft_vector_init(&buffer, sizeof(char), map->heigh * (map->width + 1), NULL))
+		return (FAILURE);
 	i = 0;
 	while (i < map->heigh)
 	{
 		j = 0;
 		while (j < map->width)
 		{
-			write(1, map->c + map->map[(i * map->width) + j], 1);
+			if (ft_vector_add(&buffer, map->c + map->map[(i * map->width) + j]))
+				return (FAILURE);
 			j++;
 		}
-		write(1, "\n", 1);
+		if (ft_vector_add(&buffer, "\n"))
+			return (FAILURE);
 		i++;
 	}
-	return (0);
+	write(1, buffer.ptr, buffer.total);
+	ft_vector_free(&buffer);
+	return (SUCCESS);
 }
