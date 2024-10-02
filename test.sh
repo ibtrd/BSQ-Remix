39d6bab0fba5715ca8871344b0da7573aa9558e2
Michel Juffard <143285381+MichJuffard@users.noreply.github.com>
2024-10-02 17:07:45 +0200

    update generator

--- test.sh
+++ test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-while [ 1 ]
+set -o pipefail
+while :
 do
-	./generator 20 50 2 | ./bsq
-	echo ---------------------------------------------------------------------------------
-	sleep 0.5
+    X=5
+    Y=70
+    RANGE=$((Y-X+1))
+    R1=$(($(($RANDOM%$RANGE))+X))
+    R2=$(($(($RANDOM%$RANGE))+X))
+    if [ $R1 -gt $R2 ]
+    then
+        RANGE2=$R2
+    else
+        RANGE2=$R1
+    fi
+    R3=$(($(($RANDOM%$RANGE2))+1))
+
+    ./generator $R1 $R2 $R3 > map
+    cat map
+    echo && echo
+    ./bsq map
+    sleep 1
+	echo && echo
 done
